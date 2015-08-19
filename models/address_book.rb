50b330e05ae7206b6fb51295795d53569251ed36
Jon Malis <jmalis@live.com>
2015-08-19 16:41:18 -0700

    address-bloc-search-checkpoint

--- models/address_book.rb
+++ models/address_book.rb
@@ -48,4 +48,29 @@ class AddressBook
               puts "The number you selected is not valid"
             end
       end
+
+    def binary_search(name)
+      #save the index of the leftmost item in the array in a variable named lower, and the index of rightmost item in the array in upper. If we think of the array in terms of left-to-right where the leftmost item is the zeroth index and the rightmost item is the entries.length-1 index.
+      lower = 0
+      upper = entries.length - 1
+
+     #we loop while our lower index is less than or equal to our upper index.
+     while lower <= upper
+     #we find the middle index by taking the sum of lower and upper and dividing it by two. Ruby will truncate any decimal numbers, so if upper is five and lower is zero then mid will get set to two. Then we retrieve the name of the entry at the middle index and store it in mid_name.
+       mid = (lower + upper) / 2
+       mid_name = entries[mid].name
+
+     #we compare the name that we are searching for, name, to the name of the middle index, mid_name. Take note that we are using the == operator when comparing the names which makes the search case sensitive
+       if name == mid_name
+         return entries[mid]
+       elsif name < mid_name
+         upper = mid - 1
+       elsif name > mid_name
+         lower = mid + 1
+       end
+     end
+     #if we divide and conquer to the point where no match is found, we simply return nil.
+     return nil
+    end
+
 end
