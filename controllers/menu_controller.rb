3fb383f848053fc1233f497096e4a5604984196e
Jon Malis <jmalis@live.com>
2015-08-19 16:03:08 -0700

    commit full assignment

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -2,13 +2,13 @@ require_relative "../models/address_book.rb"
 
 class MenuController
     attr_accessor :address_book
-    
+
     def initialize
         @address_book = AddressBook.new
     end
-    
+
     def main_menu
-        
+
         puts "Main Menu - #{@address_book.entries.count} entries"
         puts "1 - View all entries"
         puts "2 - View Entry Number n"
@@ -17,11 +17,11 @@ class MenuController
         puts "5 - Import entries from a CSV"
         puts "6 - Exit"
         print "Enter your selection: "
-        
-        
+
+
         selection = gets.to_i
         puts "You picked #{selection}"
-        
+
         case selection
             when 1
             system "clear"
@@ -45,34 +45,34 @@ class MenuController
             main_menu
             when 6
             puts "Good-bye!"
-            
+
             exit(0)
-            
+
             else
             system "clear"
             puts "Sorry, that is not a valid input"
             main_menu
         end
     end
-    
-    
+
+
         def view_all_entries
             @address_book.entries.each do |entry|
                 system "clear"
                 puts entry.to_s
-            
+
                 entry_submenu(entry)
             end
-            
+
             system "clear"
             puts "End of entries"
         end
-        
+
         def view_entry_number
             print "Entry number to view: "
-            
+
                 selection = gets.chomp.to_i
-            
+
                 if selection < @address_book.entries.count
                   puts @address_book.entries[selection]
                   puts "Press enter to return to the main menu"
@@ -83,49 +83,49 @@ class MenuController
                   view_entry_number
                 end
         end
-                
-            
-    
+
+
+
         def create_entry
             system "clear"
             puts "New AddressBloc Entry"
-            
+
             print "Name: "
             name = gets.chomp
             print "Phone number: "
             phone = gets.chomp
             print "Email: "
             email = gets.chomp
-            
-            
+
+
             @address_book.add_entry(name, phone, email)
-            
+
             system "clear"
             puts "New entry created"
         end
-        
+
         def search_entries
         end
-    
+
         def read_csv
         end
-        
+
         def entry_submenu(entry)
-            
+
             puts "n - next entry"
             puts "d - delete entry"
             puts "e - edit this entry"
             puts "m - return to main menu"
-            
-            
+
+
             selection = gets.chomp
-            
+
             case selection
-                
+
                 when "n"
                 when "d"
                 when "e"
-                
+
                 when "m"
                 system "clear"
                 main_menu
@@ -136,4 +136,3 @@ class MenuController
             end
         end
 end
-
--- models/address_book.rb
+++ models/address_book.rb
@@ -1,12 +1,13 @@
 require_relative "entry.rb"
+require "csv"
 
 class AddressBook
     attr_accessor :entries
-    
+
     def initialize
         @entries = []
     end
-    
+
     def add_entry(name, phone, email)
         index = 0
         @entries.each do |entry|
@@ -17,7 +18,18 @@ class AddressBook
         end
         @entries.insert(index, Entry.new(name, phone, email))
     end
-    
+
+    def import_from_csv(file_name)
+      csv_text = File.read(file_name)
+     csv = CSV.parse(csv_text, headers: true, skip_blanks: true)
+
+     csv.each do |row|
+       row_hash = row.to_hash
+       add_entry(row_hash["name"], row_hash["phone_number"], row_hash["email"])
+     end
+
+    end
+
     def remove_entry(name, phone, email)
         index = 1
         @entries.each do |entry|
@@ -27,7 +39,7 @@ class AddressBook
         end
         index = 0
     end
-    
+
     def view_entry_number(index)
             if index >= 0 && index < entries.size
               return entries[index]
